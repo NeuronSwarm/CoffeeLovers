271437403b595343231e2e5b4c8212bac87b2ab0
Ty Connors <tconn89@gmail.com>
2017-12-18 11:06:46 -0500

    Production config

--- client/config.js
+++ client/config.js
@@ -1,5 +1,12 @@
+var url;
+if(process.env.NODE_ENV == 'production'){
+  url = 'https://techdrone.us'
+}
+else {
+  url = 'http://localhost:3000'
+}
 export default {
   site: {
-    url: 'http://localhost:3000'
+    url: url
   }
 }
