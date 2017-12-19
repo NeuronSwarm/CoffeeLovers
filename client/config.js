dc8391b224179fe54409146f10f163cae72de716
Ty Connors <tconn89@gmail.com>
2017-12-19 15:16:05 -0500

    Fix url

--- client/config.js
+++ client/config.js
@@ -1,8 +1,8 @@
 var url = (process.env.NODE_ENV == 'production') ?
-  'https://techdrone.us/' : 'http://localhost:3000/'
+  'https://techdrone.us' : 'http://localhost:3000'
 
 var api = (process.env.NODE_ENV == 'production') ?
-  'https://techdrone.us/' : 'http://localhost:8080/'
+  'https://techdrone.us' : 'http://localhost:8080'
 
 export default {
   site: { url, api }
